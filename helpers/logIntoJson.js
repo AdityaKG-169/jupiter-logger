2233e07d426039deddcd8e271f9119cca0310ac4
AdityaKG-169 <aditya.kg169@gmail.com>
2021-10-28 13:17:21 +0530

    fix: logintojson function fixes

--- helpers/logIntoJson.js
+++ helpers/logIntoJson.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 
 const logIntoJson = (body, statusCode) => {
   const objectToStore = {
-    responseBody: body,
+    responseBody: body.replace(/\\"/g, '"'),
     statusCode,
     logCreatedAt: new Date(),
   };
 
   // log objectToStore into json file
-  fs.appendFile('./logs/log.json', JSON.stringify(objectToStore), (err) => {
+  fs.writeFile('log.json', JSON.stringify(objectToStore), (err) => {
     if (err) throw err;
   });
 };
